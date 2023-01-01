c3aeb463c6966ef6d4a758d691fd8b08a3568b26
Qijia Liu <liumeo@pku.edu.cn>
2023-01-01 10:55:54 -0500

    parallel build

--- scripts/build_lib.sh
+++ scripts/build_lib.sh
@@ -1,6 +1,7 @@
 set -e
+n=`python -c 'import multiprocessing as mp; print(mp.cpu_count())'`
+
 cd openssl
 emconfigure ./Configure gcc
 sed -i.bak s/^CROSS_COMPILE=.*/CROSS_COMPILE=/ Makefile && rm Makefile.bak
-make clean
-emmake make build_generated libcrypto.a
+emmake make -j $n build_generated libcrypto.a
